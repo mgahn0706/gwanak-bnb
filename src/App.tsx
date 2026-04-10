28963f82bb5c74bf8928296b5e70a48f3cf2397d
mgahn0706 <mgahn0706@snu.ac.kr>
2026-04-11 00:01:53 +0900

    feat: Add LocationSearchBar component to App for enhanced location filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import GuestFilterSelectPopover from "./components/GuestFilterSelectPopover";
+import LocationSearchBar from "./components/LocationSearchBar";
 
 function App() {
   return (
     <section id="center">
+      <LocationSearchBar />
       <GuestFilterSelectPopover />
     </section>
   );
